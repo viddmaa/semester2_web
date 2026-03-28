712ea47eb44c38dc44bd05fe63c2005e595aaccf
Valentina Kholod <valentina.kholod@yandex.ru>
2026-03-28 15:39:35 +0300

    Task 4. DTL, add new page

--- catlibrary/urls.py
+++ catlibrary/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from catlibrary.views import index
+from . import views
 
 app_name = "catlibrary"
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', views.index, name='home'),
+    path('care/', views.care, name='care'),
+    path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
+
 ]
